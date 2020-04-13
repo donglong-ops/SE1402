e1ee5eacc2e558124aa63e1e81e9909fa829e3ac
donglong-ops <donglong453@gmail.com>
2020-04-13 08:44:35 +0700

    1st upload

---
+++ src/se1402/SE1402.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se1402;
+
+import utils.MyToys;
+
+/**
+ *
+ * @author donglong
+ */
+public class SE1402 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("5!= " + MyToys.cF(5)); // kỳ vọng 120
+        System.out.println("6!= " + MyToys.cF(6)); // kỳ vọng 720
+        System.out.println("0!= " + MyToys.cF(0)); // kỳ vọng 1
+        System.out.println("Exeption: " + MyToys.cF(-5)); // kỳ vọng bị sml
+                                //bị ném mặt cái ngoại lệ
+    }
+    
+}
---
+++ src/utils/MyToys.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+/**
+ *
+ * @author donglong
+ */
+public class MyToys {
+    //bộ thư viện các hàng xài chung, cho nên chúng là static
+    
+    //cF: compute factorial, tính n giai thừa n != 1.2.3...n
+    //0!  = 1
+    //
+    public static long cF(int n){
+        if(n < 0){
+            throw new IllegalArgumentException("Sorry, n must be >= 0");
+        }
+        if(n == 0){
+            return 1;
+        }
+        //phần còn lại hợp lệ, tính bt
+        long product = 1;
+        for (int i = 1; i < n; i++) {
+            product *= i;
+            
+        }
+        return product;
+        
+    }
+    
+}
